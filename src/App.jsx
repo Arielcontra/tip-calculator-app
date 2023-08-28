6a05ae0724e3a05f0d36dac7d99e0f21031c81f5
Ariel Contreras <arielcontra@hotmail.com>
2023-08-28 19:15:03 -0300

    last details layout

--- src/App.jsx
+++ src/App.jsx
@@ -7,32 +7,30 @@ function App() {
 
   return (
     <>
-      <div className='wrapperApp'>
-        <header className='header'>
-          <h1>SPLIT THE BILL</h1>
-        </header>
-        <div className='modal'>
-          <div className='wrapperSlider-Display'>
-            <Slider />
-            <Display />
-          </div>
+      <header className='header'>
+        <h1>SPLIT THE BILL</h1>
+      </header>
+      <div className='modal'>
+        <div className='wrapperSlider-Display'>
+          <Slider />
+          <Display />
         </div>
-        <footer className='attribution'>
-          <p className='link'>
-            Challenge by&nbsp;
-            <a href='https://www.frontendmentor.io?ref=challenge' target={'_blank'} rel='noreferrer'>
-              Frontend Mentor
-            </a>.
-            Coded by&nbsp;
-            <a href='https://www.linkedin.com/in/ariel-contreras-599ab921b' target={'_blank'} rel='noreferrer'>
-              Ariel Contreras.
-            </a>
-            <a href='https://github.com/Arielcontra/tip-calculator-app' target={'_blank'} rel='noreferrer'>
-              <img src={gitHub} alt='Logo github' className='logoGitHub' />
-            </a>
-          </p>
-        </footer>
       </div>
+      <footer className='attribution'>
+        <p className='link'>
+          Challenge by&nbsp;
+          <a href='https://www.frontendmentor.io?ref=challenge' target={'_blank'} rel='noreferrer'>
+            Frontend Mentor
+          </a>.
+          Coded by&nbsp;
+          <a href='https://www.linkedin.com/in/arielcontra/' target={'_blank'} rel='noreferrer'>
+            Ariel Contreras.
+          </a>
+          <a href='https://github.com/Arielcontra/tip-calculator-app' target={'_blank'} rel='noreferrer'>
+            <img src={gitHub} alt='Logo github' className='logoGitHub' />
+          </a>
+        </p>
+      </footer>
     </>
   )
 }
